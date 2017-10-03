ec7b33228bcaf678833412609c9c9a8f7ddd3301
ashwani <ashwani.singh@opstree.com>
2017-10-04 02:08:12 +0530

    .

--- prometheus/recipes/default.rb
+++ prometheus/recipes/default.rb
@@ -4,13 +4,18 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-remote_file 'Prometheus_download' do
-  path '/tmp/prometheus-1.7.2.linux-amd64.tar.gz'
-  source 'https://github.com/prometheus/prometheus/releases/download/v1.7.2/prometheus-1.7.2.linux-amd64.tar.gz'
+#remote_file 'Prometheus_download' do
+#  path '/tmp/prometheus-1.7.2.linux-amd64.tar.gz'
+#  source 'https://github.com/prometheus/prometheus/releases/download/v1.7.2/prometheus-1.7.2.linux-amd64.tar.gz'
+#  mode '0755'
+#  action :create
+#end
+
+cookbook_file '/tmp/prometheus-1.7.2.linux-amd64.tar.gz' do
+  source 'prometheus-1.7.2.linux-amd64.tar.gz'
   mode '0755'
   action :create
 end
-
 
 execute 'untar prometheus' do
   command 'tar -xzf /tmp/prometheus-1.7.2.linux-amd64.tar.gz -C /opt; ln -s /opt/prometheus-1.7.2.linux-amd64 /opt/prometheus'
