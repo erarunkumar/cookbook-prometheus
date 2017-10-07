a5c3babcbaefd44254fe2a37a9a461be0ed53e1d
ashwani <ashwani.singh@opstree.com>
2017-10-07 22:35:58 +0530

    .

--- cookbooks/prometheus_wrapper/recipes/default.rb
+++ cookbooks/prometheus_wrapper/recipes/default.rb
@@ -2,13 +2,6 @@
 
 include_recipe "prometheus::use_lwrp"
 
-# Add a rule filename under `rule_files` in prometheus.yml.erb
-
-# Example of using search to populate prometheus.yaml jobs using the prometheus_job LWRP
-# Finds all the instances that are in the current environment and are taged with "node_exporter"
-# Assumes that the service instances were tagged in their own recipes.
-#client_servers = search(:node, "environment:#{node.chef_environment} AND tags:node_exporter")
-
 # Assumes service_name is an attribute of each node
 client_servers.each do |server|
 	prometheus_job server['service_name'] do
