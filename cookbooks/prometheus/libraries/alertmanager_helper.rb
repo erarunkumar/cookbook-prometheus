717d73c79cb4665fad66760c913d99366ad35c79
saurabhvaj <saurabhvajpayee2010@gmail.com>
2017-10-10 18:07:01 +0530

    created a new branch fro fresh code

--- cookbooks/prometheus/libraries/alertmanager_helper.rb
+++
@@ -1,8 +0,0 @@
-
-def generate_alertmanager_flags
-  config = ''
-  node['prometheus']['alertmanager']['flags'].each do |flag_key, flag_value|
-    config += "-#{flag_key}=#{flag_value} " if flag_value != ''
-  end
-  config
-end
